7cf2c35b5e52d0e6dd0ddec6c4255db88646c543
Dena <denaalt2004@gmail.com>
2024-08-05 19:15:26 +0300

    done

--- index.js
+++ index.js
@@ -7,5 +7,24 @@ function calculateBMI() {
   let female = document.getElementById("f").checked;
   console.table({ age, weight, height, male, female });
 
+  height = height / 100;
+  BMI = weight / (height * height);
   // You are going to write the code here before the curly braces 👇🏻
+
+  alert(BMI);
+  // let Underweight = 18.5
+  // let Healthy Weight = 18.5 – 24.9
+  // let Overweight= 25.0 – 29.9
+  // let Above Obesity=30.0 
+  // <--!2-->
+
+  if(weight>=18.5){
+    console.log("you are Underweight")
+  }
+  else if(Healthy eight>=18.5 || Healthy Weight>=24.9)
+    console.log("you are healthy")
+  
+    else(Overweight>=25.0 && Overweight>24.9 && Above Obesity=30.0 )
+    console.log("Above Obesity")
+
 }
